3c7f4ce690fa9332e8aef227d77b3e446f570e43
Hyperspace146 <adriandinh146@gmail.com>
2023-03-21 01:57:55 -0700

    Implemented player movement

--- entities/player/player_controller.gd
+++ entities/player/player_controller.gd
@@ -1,10 +1,17 @@
-extends Node
+extends CharacterBody2D
 
-## player_controller reads controller/keyboard input and allows characters to perform actions.
+## Reads controller/keyboard input and allows characters to perform actions.
 
-func _input(event):
-	var velocity = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+## Player speed while moving. 
+const move_speed = 2
 
+
+func _physics_process(delta):
+	# Move the character every physics frame
+	_move()
+
+
+# Moves the character in a direction according to input. Acknowledges wall collision.
 func _move():
-	var velocity = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	
+	var velocity = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	move_and_collide(velocity * move_speed)
